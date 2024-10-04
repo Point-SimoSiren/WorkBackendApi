5c1fc6f2fe53ebe0763bdd7019da855a28939a52
Pont-SimoSiren <sirensimo7@gmail.com>
2024-10-04 14:29:25 +0300

    Add project files.

---
+++ WorkBackendApi/Controllers/EmployeesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WorkBackendApi.Models;
+
+namespace WorkBackendApi.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+
+        private readonly tuntidbContext db = new tuntidbContext();
+
+        [HttpGet]
+        public ActionResult GetEmployees()
+        {
+            var emp = db.Employees.Where(e => e.Active == true);
+            return Ok(emp);
+        }
+
+    }
+}
---
+++ WorkBackendApi/Controllers/WorkassignmentsController.cs
@@ -0,0 +1,61 @@
+﻿using MauiTimeApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WorkBackendApi.Models;
+
+namespace WorkBackendApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WorkassignmentsController : ControllerBase
+    {
+        private readonly tuntidbContext db =  new tuntidbContext();
+
+
+        [HttpGet]
+        public ActionResult GetWa()
+        {
+            var wa = db.WorkAssignments.Where(e => e.Active == true);
+            return Ok(wa);
+        }
+
+        // START
+        [HttpPost("start")]
+        public bool Start([FromBody] Operation op) {
+
+            // Haetaan tietokannassa oleva WA rivi
+            WorkAssignment? wa = db.WorkAssignments.Find(op.WorkAssignmentID);
+
+            // Jos työtehtävä on jo käynnissä sitä ei voi aloittaa ja palautetaan false mobiilisovellukselle
+            if ( wa.InProgress == true)
+            {
+                return false;
+            }
+
+            // Jos kaikki ok, muokataan WA riviä ja tallennetaan tietokantaan
+            wa.InProgress = true;
+            wa.WorkStartedAt = DateTime.Now.AddHours(1);
+            db.SaveChanges();
+
+            // Luodaan uusi TimeSheet rivi tietokantaan
+            Timesheet newEntry = new Timesheet()
+            {
+                IdWorkAssignment = op.WorkAssignmentID,
+                StartTime = DateTime.Now.AddHours(1),
+                Active = true,
+                IdEmployee = op.EmployeeID,
+                IdCustomer = wa.IdCustomer,
+                CreatedAt = DateTime.Now.AddHours(1),
+                Comments = op.Comment
+            };
+
+            db.Timesheets.Add(newEntry);
+
+            db.SaveChanges();
+
+            return (true);
+
+        }
+
+    }
+}
